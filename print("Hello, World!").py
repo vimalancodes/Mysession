ae803bbfddfaf7abe74a00c04927542d09b163a6
vimalan Sathyanarayanan <vxsathyanarayanan@shockers.wichita.edu>
2024-06-19 00:03:00 -0500

    Initial commit

---
+++ print("Hello, World!").py
@@ -0,0 +1,18 @@
+print("Hello, World!")
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            next_num = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_num)
+        return fib_sequence
+
+n = 10
+fib_sequence = fibonacci(n)
+print(fib_sequence)
